e1be8a7669f44923788ad49219637cdfc5e71163
Bezalel Leyian <158043641+LeyianB24@users.noreply.github.com>
2026-03-13 11:20:06 +0300

    config: Add test environment configuration file.

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :chat_app, ChatApp.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "7136",
   hostname: "localhost",
   database: "chat_app_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
