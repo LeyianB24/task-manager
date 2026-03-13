e427ae1e5e54084692d39eff2aec1af9d277656d
Bezalel Leyian <158043641+LeyianB24@users.noreply.github.com>
2026-03-13 10:48:43 +0300

    feat: Implement workspace resource with full CRUD operations, API endpoints, and associated tests and database migration.

---
+++ backend/test/chat_app_web/controllers/workspace_controller_test.exs
@@ -0,0 +1,88 @@
+defmodule ChatAppWeb.WorkspaceControllerTest do
+  use ChatAppWeb.ConnCase
+
+  import ChatApp.WorkspacesFixtures
+
+  alias ChatApp.Workspaces.Workspace
+
+  @create_attrs %{
+    description: "some description",
+    name: "some name"
+  }
+  @update_attrs %{
+    description: "some updated description",
+    name: "some updated name"
+  }
+  @invalid_attrs %{description: nil, name: nil}
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "index" do
+    test "lists all workspaces", %{conn: conn} do
+      conn = get(conn, ~p"/api/workspaces")
+      assert json_response(conn, 200)["data"] == []
+    end
+  end
+
+  describe "create workspace" do
+    test "renders workspace when data is valid", %{conn: conn} do
+      conn = post(conn, ~p"/api/workspaces", workspace: @create_attrs)
+      assert %{"id" => id} = json_response(conn, 201)["data"]
+
+      conn = get(conn, ~p"/api/workspaces/#{id}")
+
+      assert %{
+               "id" => ^id,
+               "description" => "some description",
+               "name" => "some name"
+             } = json_response(conn, 200)["data"]
+    end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = post(conn, ~p"/api/workspaces", workspace: @invalid_attrs)
+      assert json_response(conn, 422)["errors"] != %{}
+    end
+  end
+
+  describe "update workspace" do
+    setup [:create_workspace]
+
+    test "renders workspace when data is valid", %{conn: conn, workspace: %Workspace{id: id} = workspace} do
+      conn = put(conn, ~p"/api/workspaces/#{workspace}", workspace: @update_attrs)
+      assert %{"id" => ^id} = json_response(conn, 200)["data"]
+
+      conn = get(conn, ~p"/api/workspaces/#{id}")
+
+      assert %{
+               "id" => ^id,
+               "description" => "some updated description",
+               "name" => "some updated name"
+             } = json_response(conn, 200)["data"]
+    end
+
+    test "renders errors when data is invalid", %{conn: conn, workspace: workspace} do
+      conn = put(conn, ~p"/api/workspaces/#{workspace}", workspace: @invalid_attrs)
+      assert json_response(conn, 422)["errors"] != %{}
+    end
+  end
+
+  describe "delete workspace" do
+    setup [:create_workspace]
+
+    test "deletes chosen workspace", %{conn: conn, workspace: workspace} do
+      conn = delete(conn, ~p"/api/workspaces/#{workspace}")
+      assert response(conn, 204)
+
+      assert_error_sent 404, fn ->
+        get(conn, ~p"/api/workspaces/#{workspace}")
+      end
+    end
+  end
+
+  defp create_workspace(_) do
+    workspace = workspace_fixture()
+    %{workspace: workspace}
+  end
+end
